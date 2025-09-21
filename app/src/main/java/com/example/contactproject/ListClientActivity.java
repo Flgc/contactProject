e7ab969070c0e61abf3e29faac6e37d9000137eb
Fábio Luís <ml@fabiotecnico1.biz>
2025-09-21 11:34:08 -0300

    Activity for module 04 - contact registration screen implemented

--- app/src/main/java/com/example/contactproject/ListClientActivity.java
+++ app/src/main/java/com/example/contactproject/ListClientActivity.java
@@ -13,20 +13,44 @@ import com.example.contactproject.adapter.ContactAdapter;
 import com.example.contactproject.dao.ContactDAO;
 import com.example.contactproject.model.Contact;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListClientActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activityclientlist);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loadContacts();
     }
+
+    private void loadContacts(){
+        ContactDAO contactDAO = new ContactDAO(this);
+
+        /*
+        Contact c1 = new Contact(1,"Contact 01");
+        Contact c2 = new Contact(1,"Contact 02");
+        Contact c3 = new Contact(1,"Contact 03");
+
+        contactDAO.contactInsert(c1);
+        contactDAO.contactInsert(c2);
+        contactDAO.contactInsert(c3);
+
+         */
+
+        List<Contact> contacts = contactDAO.listContacts();
+
+        ContactAdapter contactAdapter = new ContactAdapter(contacts, this);
+
+        ListView lvContacts = findViewById(R.id.lvContacts);
+        lvContacts.setAdapter(contactAdapter);
+    }
+
 }
